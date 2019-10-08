d2c3251c5828c3f836c4e3bfdbc187791e596d99
NathanKewley <nathan.kewley@gmail.com>
2019-10-08 22:30:57 +1100

    trying to add token types... hitting errors, done for today tho

--- Token.cs
+++ Token.cs
@@ -4,8 +4,22 @@ namespace emlang
 {
 	public class Token
 	{
-		public Token()
+		TokenType type;
+		string lexme;
+		object literal;
+		int line;
+
+		public Token(TokenType type, string lexme, object literal, int line)
 		{
+			this.type = type;
+			this.lexme = lexme;
+			this.literal = literal;
+			this.line = line;
+		}
+
+		public string toString()
+		{
+			return($"{type} {lexme} {literal}");
 		}
 	}
 }
