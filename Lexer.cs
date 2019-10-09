2adf575381c6cdfc243266520788136dff254ffd
NathanKewley <nathan.kewley@gmail.com>
2019-10-09 17:41:20 +1100

    updated to lexer

--- Lexer.cs
+++ Lexer.cs
@@ -5,16 +5,29 @@ namespace emlang
 {
 	public class Lexer
 	{
-		string code;
+		List<Token> tokens = new List<Token>();
+		string source;
+		int start = 0;
+	    int current = 0; 
+		int line = 1;
 
-		public Lexer(string inCode)
+		public Lexer(string code)
 		{
-			code = inCode;
+			this.source = code;
+		}
+
+		private bool isAtEnd(){
+			return(current >= source.Length);
 		}
 
 		public List<Token> lex()
 		{
-			List<Token> tokens = new List<Token>();
+			while(!isAtEnd()){
+				start = current;
+				scanToken();
+			}
+
+			tokens.Add(new Token(TokenType.EOF, "", null, line));
 			return(tokens);
 		}
 	}
